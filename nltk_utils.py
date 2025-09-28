ee0cac719d78b0d297731d130dee8600853ce1cb
Yung Tyro <bandabernard917@gmail.com>
2025-09-28 16:58:48 +0200

    implemented the functionality of the bag of words with numpy

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,5 @@
 import nltk
-
+import numpy as np
 # Downloading tokenizer
 #nltk.download('punkt')
 #nltk.download('punkt_tab') IF PUNKT DOESN'T WORK USE PUNKT_TAB
@@ -21,5 +21,12 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
 
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+        return bag
